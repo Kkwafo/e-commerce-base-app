5c3c47089c5aa0525a7edaff8bc3434268f45689
Kkwafo <kkwafoawua@gmail.com>
2024-04-28 20:28:43 -0300

    adding Table

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { PageHeader } from '../_components/PageHeader'
 import Link from 'next/link'
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 export default function AdminProductsPage() {
   return (
@@ -11,6 +12,31 @@ export default function AdminProductsPage() {
           <Link href="/admin/products/new"> Add Product</Link>
         </Button>
       </div>
+      <ProductTable />
     </>
   )
+}
+
+function ProductTable(){
+  return <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead className="w-0">
+          <span className='sr-only'>
+            Avaible For Purchase
+          </span>
+        </TableHead>
+        <TableHead>Name</TableHead>
+        <TableHead>Price</TableHead>
+        <TableHead>Orders</TableHead>
+        <TableHead className="w-0">
+          <span className='sr-only'>
+            Actions
+          </span>
+        </TableHead>      </TableRow>
+    </TableHeader>
+    <TableBody>
+      
+    </TableBody>
+  </Table>
 }
