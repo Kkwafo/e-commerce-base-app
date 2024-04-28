1fca87111e3740499fc8674ba5897483316f1c1f
Kkwafo <kkwafoawua@gmail.com>
2024-04-28 03:18:22 -0300

    add card

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,18 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 export default function AdminDashboard() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">page</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle> Sales</CardTitle>
+          <CardDescription>desc</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Text</p>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
